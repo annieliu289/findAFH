e43d5340cfe25303070d9babcf5161cc87f1cea6
seliom <gobezes22@wlu.edu>
2021-07-18 19:35:04 -0700

    Removed text under dropdown and made just homeListing widget scrollable

--- lib/Screens/listings_map_screen.dart
+++ lib/Screens/listings_map_screen.dart
@@ -1,4 +1,5 @@
 import 'package:find_afh/Screens/components/left_Column.dart';
+import 'package:find_afh/city_dropdown.dart';
 import 'package:flutter/material.dart';
 
 import '../Models/listing.dart';
@@ -37,40 +38,100 @@ class ListingsMapScreen extends StatelessWidget {
       image: 'assets/images/afhStockImage.jpeg',
       availableBeds: 6,
     ),
+    Listing(
+      name: 'Grace Adult Family Home',
+      city: 'Vancouver',
+      address: '15575 NE 11th Street, Vancouver, WA 98684',
+      rating: '5/5',
+      contact: 3605139667,
+      image: 'assets/images/afhStockImage.jpeg',
+      availableBeds: 0,
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
     return Scaffold(
       backgroundColor: kPlainColor,
       extendBodyBehindAppBar: true,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-                margin: EdgeInsets.fromLTRB(100, 50, 100, 0), child: TopBar()),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Container(
-                    margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          LeftColumn(),
-
-                          // mapping homes from data to widgets
-                          Container(
-                            margin: EdgeInsets.fromLTRB(75, 0, 0, 0),
-                            constraints:
-                                BoxConstraints.loose(Size.square(1200)),
-                            child: Container(
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                    child: Card(
+      body: Column(
+        children: [
+          Container(
+              margin: EdgeInsets.fromLTRB(100, 50, 100, 0), child: TopBar()),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                  child: Column(
+                    //crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //LeftColumn(),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(0, 0, 625, 20),
+                        child: Text(
+                          'Location',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: kPrimaryColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                          ),
+                        ),
+                      ),
+                      Container(
+                          height: 70,
+                          width: mediaQuery.size.width * 0.4,
+                          child: Stack(
+                            children: [
+                              Container(
+                                height: 50,
+                                width: mediaQuery.size.width * 0.3,
+                                child: CityDropdownWidget(),
+                              ),
+                              Container(
+                                height: 50,
+                                width: mediaQuery.size.width * 0.3,
+                                padding: EdgeInsets.only(
+                                    left: mediaQuery.size.width * .15),
+                                child: ElevatedButton(
+                                  onPressed: () => {},
+                                  style: ElevatedButton.styleFrom(
+                                      primary: kPrimaryColor,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(0),
+                                      )),
+                                  child: Row(
+                                    children: [
+                                      IconButton(
+                                        icon: const Icon(Icons.search),
+                                        onPressed: () {},
+                                      ),
+                                      const Text(
+                                        'Find Homes',
+                                        textAlign: TextAlign.end,
+                                        style: TextStyle(fontSize: 24),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )),
+                      // mapping homes from data to widgets
+                      Container(
+                        margin: EdgeInsets.fromLTRB(70, 0, 0, 0),
+                        constraints: BoxConstraints.loose(Size.square(1200)),
+                        child: Container(
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Card(
+                                  child: Container(
+                                    height: 670,
+                                    child: SingleChildScrollView(
                                       child: Column(
                                         children: homeListings.map((ls) {
                                           return Container(
@@ -143,32 +204,32 @@ class ListingsMapScreen extends StatelessWidget {
                                       ),
                                     ),
                                   ),
-                                ],
+                                ),
                               ),
-                            ),
+                            ],
                           ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                    child: Container(
-                  constraints: BoxConstraints.tight(Size(800, 800)),
-                  margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
-                  child: Column(
-                    children: [
-                      Text('This is where the Map View will be:'),
-                      Placeholder(
-                        fallbackHeight: 700,
+                        ),
                       ),
                     ],
                   ),
-                )),
-              ],
-            ),
-          ],
-        ),
+                ),
+              ),
+              Expanded(
+                  child: Container(
+                constraints: BoxConstraints.tight(Size(800, 800)),
+                margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                child: Column(
+                  children: [
+                    Text('This is where the Map View will be:'),
+                    Placeholder(
+                      fallbackHeight: mediaQuery.size.height * .75,
+                    ),
+                  ],
+                ),
+              )),
+            ],
+          ),
+        ],
       ),
     );
   }
